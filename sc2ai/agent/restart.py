ab88ce3b6521c066c4f1560ecf3a1618da772ffa
Torsten <tw@mitscom.de>
2019-01-08 08:41:21 +0100

    Restart Script

--- sc2ai/agent/restart.py
+++ sc2ai/agent/restart.py
@@ -4,5 +4,8 @@ import time
 while 1:
     grep = os.system("ps -ef | grep pysc2.bin.agent")
     if grep == 0:
-        os.system("python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
+        # os.system("python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
+        print('Not Running')
+    else:
+        print('Grep: '+grep)
     time.sleep(1)
