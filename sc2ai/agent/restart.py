c430227d5b6274fd895ce839ff0b611b23dc0d9d
Torsten <tw@mitscom.de>
2019-01-08 08:53:46 +0100

    Restart Agent

--- sc2ai/agent/restart.py
+++ sc2ai/agent/restart.py
@@ -1,11 +1,28 @@
 import os
+import psutil
 import time
 
-while 1:
-    grep = os.system("ps -ef | grep pysc2.bin.agent")
-    if grep == 0:
-        # os.system("python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
-        print('Not Running')
-    else:
-        print('Grep: '+grep)
-    time.sleep(1)
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    # Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+
+if __name__ == '__main__':
+
+    while 1:
+        if not checkIfProcessRunning('pysc2.bin.agent'):
+            print('No chrome process was running')
+            # os.system("python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
+
+        time.sleep(1)
