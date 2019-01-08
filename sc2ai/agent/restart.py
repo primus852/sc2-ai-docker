a052bbe6bb5d63f05f827dda4cd728b33e827f7f
Torsten <tw@mitscom.de>
2019-01-08 08:39:28 +0100

    Restart the Agent

--- sc2ai/agent/restart.py
+++ sc2ai/agent/restart.py
@@ -4,5 +4,5 @@ import time
 while 1:
     grep = os.system("ps -ef | grep pysc2.bin.agent")
     if grep == 0:
-        os.system("cd /sc2ai/agent && python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
+        os.system("python3 -m pysc2.bin.agent --map Simple64 --agent agent.refined.SparseAgent --agent_race terran --norender")
     time.sleep(1)
