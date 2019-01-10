ad7990c066d8ba1d69ff1867b2b8e0b9967af8d4
Torsten <tw@mitscom.de>
2019-01-10 11:22:17 +0100

    Neural Agent

--- sc2ai/agent/agent/neural.py
+++ sc2ai/agent/agent/neural.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 
 class NeuralBot(sc2.BotAI):
@@ -10,11 +10,32 @@ class NeuralBot(sc2.BotAI):
         """ Wait for the workers to mine """
         await self.distribute_workers()
         await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
             if self.can_afford(PROBE):
                 await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses.first)
+
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(25, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
 
 
 run_game(maps.get("AbyssalReefLE"), [
