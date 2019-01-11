5a494d587988e74750231af7d8f7334bbd157a7f
Torsten <tw@mitscom.de>
2019-01-11 16:27:50 +0100

    Neural Agent

--- sc2ai/agent/agent/neural.py
+++ sc2ai/agent/agent/neural.py
@@ -10,7 +10,7 @@ from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, \
     OBSERVER, ROBOTICSFACILITY
 
-HEADLESS = False
+HEADLESS = True
 
 class NeuralBot(sc2.BotAI):
     def __init__(self):
